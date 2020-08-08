9ec2aa0c54f98f55356e1eaf34d359e966d895f3
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-07 21:27:10 -0500

    Negative digits

--- Functions_help.py
+++ Functions_help.py
@@ -16,3 +16,22 @@ def round_to_two_places(num):
 
 # Check your answer
 q1.check()
+
+## 2
+
+# The help for round says that ndigits (the second argument) may be negative. What do you think will happen when it is? Try some examples in the following cell?
+# Can you think of a case where this would be useful?
+
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    # Replace this body with your own code.
+    # ("pass" is a keyword that does literally nothing. We used it as a placeholder
+    # because after we begin a code block, Python requires at least one line of code)
+    return round(num, ndigits = -2)
+
+# Check your answer
+q1.check()
