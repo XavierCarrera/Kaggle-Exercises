72770c4706b97e00ebe37b590243cfa950ab79df
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-08 17:30:34 -0500

    Integrer detection

--- Boolean_Conditionals.py
+++ Boolean_Conditionals.py
@@ -1,3 +1,20 @@
 from learntools.core import binder; binder.bind(globals())
 from learntools.python.ex3 import *
 
+# 1 
+
+# Your code goes here. Define a function called 'sign'
+
+def sign(n):
+    
+    if n == 0:
+        return 0
+    elif n < 0:
+        return - 1
+    else:
+        return 1
+    
+
+# Check your answer
+
+q1.check()
