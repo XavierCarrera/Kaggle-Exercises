d52e4f7fdad29b0b75e8efaa14307385df0ff085
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-08 18:00:08 -0500

    Is negative conditional

--- Boolean_Conditionals.py
+++ Boolean_Conditionals.py
@@ -38,3 +38,35 @@ def to_smash(total_candies):
 to_smash(91)
 to_smash(1)
 
+def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
+    # Don't change this code. Our goal is just to find the bug, not fix it!
+    return have_umbrella or rain_level < 5 and have_hood or not rain_level > 0 and is_workday
+
+# Change the values of these inputs so they represent a case where prepared_for_weather
+# returns the wrong answer.
+have_umbrella = False 
+rain_level = 0.0
+have_hood = False
+is_workday = False 
+
+# Check what the function returns given the current values of the variables above
+actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
+print(actual)
+
+# Check your answer
+q3.check()
+
+# 4
+
+def is_negative(number):
+    if number < 0:
+        return True
+    else:
+        return False
+
+def concise_is_negative(number):
+    return True and number < 0 # Your code goes here (try to keep it to one line!)
+
+
+# Check your answer
+q4.check()
