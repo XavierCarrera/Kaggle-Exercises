d2d1c4685bcff42c630317d3ef5edd3d019d3e8d
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-07 21:33:46 -0500

    Return candies

--- Functions_help.py
+++ Functions_help.py
@@ -35,3 +35,17 @@ def round_to_two_places(num):
 
 # Check your answer
 q1.check()
+
+## 3
+
+def to_smash(total_candies, friends=3):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    return total_candies % friends
+
+# Check your answer
+q3.check()
