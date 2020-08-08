ed7e4266c3018a5c7ae5a6a0fe5b6f1a2b0040f4
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-07 19:33:19 -0500

    Circle area solved

---
+++ Syntax_Variables_Numbers.py
@@ -0,0 +1,19 @@
+from learntools.core import binder; binder.bind(globals())
+from learntools.python.ex1 import *
+
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+____
+
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+____
+
+radius = diameter / 2
+
+area = pi * (radius **2)
+
+# Check your answer
+q1.check()
+
