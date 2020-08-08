5eabd71da4f2dd4b5b0142401bdd7e6bc071300f
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-08 18:13:05 -0500

    Boolean hotdog

--- Boolean_Conditionals.py
+++ Boolean_Conditionals.py
@@ -70,3 +70,40 @@ def concise_is_negative(number):
 
 # Check your answer
 q4.check()
+
+# 5
+
+# The boolean variables `ketchup`, `mustard` and `onion` represent whether a customer wants a particular topping on their hot dog. We want to implement a number of boolean functions that correspond to some yes-or-no questions about the customer's order. For example:
+
+def onionless(ketchup, mustard, onion):
+    """Return whether the customer doesn't want onions.
+    """
+    return not onion
+
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    return ketchup and mustard and onion
+
+# Check your answer
+q5.a.check()
+
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    return not ketchup and not mustard and not onion
+
+# Check your answer
+q5.b.check()
+
+def exactly_one_sauce(ketchup, mustard, onion):
+    """Return whether the customer wants either ketchup or mustard, but not both.
+    (You may be familiar with this operation under the name "exclusive or")
+    """
+    return ketchup and not mustard or not ketchup and mustard
+
+# Check your answer
+q5.c.check()
+
+# 6
+
