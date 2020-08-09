954fb8a1bcf67a67257c5996fb0952669141c4fc
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 13:05:57 -0500

    Len lists

--- Lists.py
+++ Lists.py
@@ -43,3 +43,17 @@ def purple_shell(racers):
     
 # Check your answer
 q3.check()
+
+## 4
+
+a = [1, 2, 3]
+b = [1, [2, 3]]
+c = []
+d = [1, 2, 3][1:]
+
+# Put your predictions in the list below. Lengths should contain 4 numbers, the
+# first being the length of a, the second being the length of b and so on.
+lengths = [len(a), len(b), len(c), len(d)]
+
+# Check your answer
+q4.check()
