a3ff980f60c8082ebf1cccd2a6b3af9601f6e8d5
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 12:44:07 -0500

    Fisrt list exercise

--- Lists.py
+++ Lists.py
@@ -1,11 +1,15 @@
 from learntools.core import binder; binder.bind(globals())
 from learntools.python.ex4 import *
 
+## 1
+
 def select_second(L):
     """Return the second element of the given list. If the list has no second
     element, return None.
     """
-    pass
+    if len(L) < 2:
+        return None
+    return L[1]
 
 # Check your answer
-q1.check()s
+q1.check()
