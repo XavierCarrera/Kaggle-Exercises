4a95e6a2345dd68f1dbe0d988cd763ca0326ede7
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 14:45:13 -0500

    Word search

--- String_dicts.py
+++ String_dicts.py
@@ -11,3 +11,27 @@ def is_valid_zip(zip_code):
 # Check your answer
 q1.check()
 
+## 2
+
+def word_search(doc_list, keyword):
+    """
+    Takes a list of documents (each document is a string) and a keyword. 
+    Returns list of the index values into the original list for all documents 
+    containing the keyword.
+
+    Example:
+    doc_list = ["The Learn Python Challenge Casino.", "They bought a car", "Casinoville"]
+    >>> word_search(doc_list, 'casino')
+    >>> [0]
+    """
+    indices = []
+    for i, doc in enumerate(doc_list):
+        tokens = doc.split()
+        normalized = [token.rstrip('.,').lower() for token in tokens]
+        if keyword.lower() in normalized:
+            indices.append(i)
+    return indices
+        
+     
+# Check your answer
+q2.check()
