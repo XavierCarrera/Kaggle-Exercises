df60f8626385c1ac164316a182cd6521ea69b012
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 15:03:33 -0500

    Blackjack libraries

---
+++ Libraries.py
@@ -0,0 +1,56 @@
+from learntools.python import jimmy_slots
+# Call the get_graph() function to get Jimmy's graph
+graph = jimmy_slots.get_graph()
+graph
+
+## 1
+
+def prettify_graph(graph):
+    """Modify the given graph according to Jimmy's requests: add a title, make the y-axis
+    start at 0, label the y-axis. (And, if you're feeling ambitious, format the tick marks
+    as dollar amounts using the "$" symbol.)
+    """
+    graph.set_title("Results of 500 slot machine pulls")
+    # Make the y-axis begin at 0
+    graph.set_ylim(bottom=0)
+    # Label the y-axis
+    graph.set_ylabel("Balance")
+    # Bonus: format the numbers on the y-axis as dollar amounts
+    # An array of the values displayed on the y-axis (150, 175, 200, etc.)
+    ticks = graph.get_yticks()
+    # Format those values into strings beginning with dollar sign
+    new_labels = ['${}'.format(int(amt)) for amt in ticks]
+    # Set the new labels
+    graph.set_yticklabels(new_labels)
+
+## 2
+
+def hand_total(hand):
+    """Helper function to calculate the total points of a blackjack hand.
+    """
+    total = 0
+    aces = 0
+    for card in hand:
+        if card in ['J', 'Q', 'K']:
+            total += 10
+        elif card == 'A':
+            aces += 1
+        else:
+
+            total += int(card)
+
+    total += aces
+
+    while total + 10 <= 21 and aces > 0:
+
+        total += 10
+        aces -= 1
+    return total
+
+def blackjack_hand_greater_than(hand_1, hand_2):
+    total_1 = hand_total(hand_1)
+    total_2 = hand_total(hand_2)
+    return total_1 <= 21 and (total_1 > total_2 or total_2 > 21)
+
+# Check your answer
+q3.check()
