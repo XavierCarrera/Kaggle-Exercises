551e70ea2d7cc14d6f0dd4f9bf37d7dacd3aeb7d
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 14:15:12 -0500

    Greater element

--- Loops_Lists.py
+++ Loops_Lists.py
@@ -1,6 +1,8 @@
 from learntools.core import binder; binder.bind(globals())
 from learntools.python.ex5 import *
 
+## 1
+
 def has_lucky_number(nums):
     """Return whether the given list of numbers is lucky. A lucky list contains
     at least one number divisible by 7.
@@ -14,3 +16,6 @@ def has_lucky_number(nums):
 
 # Check your answer
 q1.check()
+
+## 2
+
