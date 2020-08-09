ce4df2b73008ca31ccfdcc5ecbb8106698ced101
Xavier Carrera <xavier.carrera1990@gmail.com>
2020-08-09 14:17:28 -0500

    Meals loop

--- Loops_Lists.py
+++ Loops_Lists.py
@@ -19,3 +19,32 @@ q1.check()
 
 ## 2
 
+def elementwise_greater_than(L, thresh):
+    """Return a list with the same length as L, where the value at index i is 
+    True if L[i] is greater than thresh, and False otherwise.
+    
+    >>> elementwise_greater_than([1, 2, 3, 4], 2)
+    [False, False, True, True]
+    """
+    x = []
+    for i in L:
+        x.append(i > thresh)
+    return x
+
+# Check your answer
+q2.check()
+
+## 3
+
+def menu_is_boring(meals):
+    """Given a list of meals served over some period of time, return True if the
+    same meal has ever been served two days in a row, and False otherwise.
+    """
+    for i in range(len(meals)-1):
+        if meals[i] == meals[i+1]:
+            return True
+    return False
+
+# Check your answer
+q3.check()
+
